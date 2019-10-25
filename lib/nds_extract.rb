c23f7db09dc29971471d12e657eb362320a60369
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 01:36:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,11 +8,19 @@ require 'directors_database'
 def directors_totals(nds)
   result = {}
   
-  pp nds
+  nds.each do |director|
+    result[director[:name]] = 0
+    director[:movies].each do |movie|
+      result[director[:name]] += movie[:worldwide_gross]
+    end
+  end
+  
+  result
   
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+
 end
